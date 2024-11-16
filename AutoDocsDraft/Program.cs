44b2798fc1132fda782d9de27458eeca57be0d2f
Markus J <markusjosepj@gmail.com>
2024-11-15 18:00:12 -0600

    Complete some preliminary work.

--- AutoDocsDraft/Program.cs
+++ AutoDocsDraft/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Dictionary<string, List<string>> inputData =
+    new Dictionary<string, List<string>>();
+
+void InputDataInitializer()
+{
+    inputData.Add("Defendent", ["Red", "John Doe"]);
+}
+
+InputDataInitializer();
+foreach (KeyValuePair<string,List<string>> entry in inputData) {
+    Console.WriteLine(entry);
+}
