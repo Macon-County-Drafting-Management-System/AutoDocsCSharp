61087afdf2a512dabff0bbcfb9fec2b700301eb6
Markus J <markusjosepj@gmail.com>
2024-11-16 21:53:36 -0600

    Add the wording to open a word document and write to it.

--- AutoDocsDraft/Program.cs
+++ AutoDocsDraft/Program.cs
@@ -1,17 +1,23 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Word = Microsoft.Office.Interop.Word;
+
+string directory = "C:\\users\\shuff\\documents\\csharpdocs\\CSharpDocTest.docx";
+
+var wordApp = new Word.Application();
+wordApp.Visible = true;
+
+var docx = wordApp.Documents.Add();
+var selection = wordApp.Selection;
+selection.TypeText("Hello to all!");
+docx.SaveAs2(directory);
 
 
-Dictionary<string, List<string>> inputData =
-    new Dictionary<string, List<string>>();
+Dictionary<string, List<string>> inputData = new();
 
-void InputDataInitializer()
-{
-    inputData.Add("Defendent", ["Red", "John Doe"]);
+void TESTINGInitializer(){
+    inputData.Add("Red", ["Defendent", "John Doe"]);
 }
 
-InputDataInitializer();
+TESTINGInitializer();
 foreach (KeyValuePair<string,List<string>> entry in inputData) {
-    Console.WriteLine(entry);
+    Console.WriteLine($"{entry.Key}: {entry.Value[0]}, {entry.Value[1]}");
 }
